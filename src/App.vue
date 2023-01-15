f4afcfb9dd9c112d6cc712280ff078cdae2818f5
ckdgns3167 <chjung@euclidsoft.co.kr>
2023-01-15 20:13:00 +0900

    todos 데이터가 vuex의 store에서 관리되도록 변경.

--- src/App.vue
+++ src/App.vue
@@ -3,13 +3,9 @@
   <div id="app">
     <!-- 프레젠테이셔널 컴포넌트 -->
     <TodoHeader />
-    <TodoInput v-on:addTodo="addTodo" />
-    <TodoList
-      v-bind:todos="todos"
-      v-on:removeTodo="removeTodo"
-      v-on:toggleComplete="toggleComplete"
-    />
-    <TodoFooter v-on:clearTodo="clearTodo"/>
+    <TodoInput />
+    <TodoList />
+    <TodoFooter />
   </div>
 </template>
 
@@ -21,49 +17,9 @@ import TodoFooter from "./components/TodoFooter.vue";
 
 export default {
   data() {
-    return {
-      todos: [], // 데이터가 한 곳에서 관리됨.
-    };
+    return {};
   },
-  methods: {
-    addTodo(todo) {
-      const obj = { completed: false, todo, addDate: new Date() };
-      this.todos.push(obj);
-      localStorage.setItem(todo, JSON.stringify(obj));
-    },
-    removeTodo(todo, index) {
-      this.todos.splice(index, 1);
-      localStorage.removeItem(todo);
-    },
-    toggleComplete(todo, index) {
-      this.todos[index].completed = !this.todos[index].completed;
-      localStorage.removeItem(todo);
-      localStorage.setItem(todo, JSON.stringify(this.todos[index]));
-    },
-    clearTodo() {
-      this.todos = [];
-      localStorage.clear();
-    }
-  },
-  created() {
-    // console.log(localStorage)
-
-    // for (let item in localStorage) {
-    //   console.log(item);
-    // }
-
-    // for(let [key, value] of Object.entries(localStorage)) {
-    //   console.log(key)
-    //   console.log(value)
-    // }c
-    const localStorageItems = Object.values(localStorage).map((x) =>
-      JSON.parse(x)
-    );
-    localStorageItems.sort((a, b) => (a.addDate > b.addDate ? 1 : -1));
-    for (let item of localStorageItems) {
-      this.todos.push(item);
-    }
-  },
+  methods: {},
   components: {
     TodoHeader,
     TodoInput,
